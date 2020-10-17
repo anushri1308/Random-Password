bac325aeb9f27891a0c7e7340435947cadc9bb83
leeyubingdarrius <48346521+leeyubingdarrius@users.noreply.github.com>
2020-10-17 23:14:28 +0800

    Update randomPassword.py

--- randomPassword.py
+++ randomPassword.py
@@ -3,7 +3,7 @@ import random
 upper_char = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 lower_char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-special_char = ['!', '@', '#', '$', '^', '&']
+special_char = ['!', '@', '#', '$', '^', '&', '%', '*', '?']
 
 password = random.choice(upper_char) + random.choice(lower_char) + random.choice(num) + random.choice(special_char)
 print(password)
