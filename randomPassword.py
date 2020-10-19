8e47c9635113580cbdabafbe9abf6a58a7c08c82
WANGCHEN722 <52946865+WANGCHEN722@users.noreply.github.com>
2020-10-19 14:25:50 +0800

    Update randomPassword.py

--- randomPassword.py
+++ randomPassword.py
@@ -1,3 +1,3 @@
 import secrets
 
-print(''.join([secrets.choice([chr(secrets.choice([x for x in range(33,127)]))]) for y in range(int(input('Password length: ')))]))
+print(''.join([secrets.choice([chr(x) for x in range(33,127)]) for y in range(int(input('Password Length: ')))]))
