451170541370218f0f1b842e18325fb49e7d0414
WANGCHEN722 <52946865+WANGCHEN722@users.noreply.github.com>
2020-10-19 14:09:46 +0800

    Update randomPassword.py

--- randomPassword.py
+++ randomPassword.py
@@ -1,9 +1,3 @@
-import random
+import secrets
 
-upper_char = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-lower_char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-special_char = ['!', '@', '#', '$', '^', '&']
-
-password = random.choice(upper_char) + random.choice(lower_char) + random.choice(num) + random.choice(special_char)
-print(password)
+print(''.join([secrets.choice([chr(secrets.choice([x for x in range(33,127)]))]) for y in range(int(input('Password length: ')))]))
